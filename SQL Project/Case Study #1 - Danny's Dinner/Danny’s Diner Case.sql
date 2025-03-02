68be1b8de847b6ef166bd24fcd0c0ca567577373
Tú Phạm <pngoctu012@gmail.com>
2025-03-02 16:42:20 +0700

    Update Danny’s Diner Case.sql

--- SQL Project/Case Study #1 - Danny's Dinner/Danny’s Diner Case.sql
+++ SQL Project/Case Study #1 - Danny's Dinner/Danny’s Diner Case.sql
@@ -1,5 +1,5 @@
---------------------------------------------------------------------------------------------
-/* What is the total amount each customer spent at the restaurant? */
+-- 1. What is the total amount each customer spent at the restaurant?
+-- Use GROUP BY to group customers and calculate SUM of price for each of them.
 select customer_id,
     sum(price) as total_amount
 from sales
@@ -8,14 +8,17 @@ left join menu
 group by customer_id
 
 --------------------------------------------------------------------------------------------
-/* How many days has each customer visited the restaurant? */ 
+--2. How many days has each customer visited the restaurant?
+-- Use GROUP BY to group customers and COUNT order_date to find the number of days each customer visited.
 select customer_id, 
     count(distinct order_date) as number_days
 from sales
 group by customer_id
 
 --------------------------------------------------------------------------------------------
-/* What was the first item from the menu purchased by each customer? */ 
+-- 3. What was the first item from the menu purchased by each customer? 
+-- Step 1: Use row_number to rank order_date of each customers for each items
+-- Step 2: Take out the fist item of each customer by row_number = 1
 select customer_id, product_name
 from (
     select customer_id, product_name,
@@ -23,11 +26,15 @@ from (
     from sales
     left join menu 
         on sales.product_id = menu.product_id
-) as table_join
-where row_number = 1
+) as table_join -- join two table and ranking for customers by order_date column
+where row_number = 1 -- take out first item of each customers
 
 --------------------------------------------------------------------------------------------
-/* What is the most purchased item on the menu and how many times was it purchased by all customers? */
+-- 4.What is the most purchased item on the menu and how many times was it purchased by all customers? 
+-- Step 1: Count number of orders per customer (group by product_name and customer_id)
+-- Step 2: Use windown function SUM to calculate total orders per product_name
+-- Step 3: Ranking total orders per product name by dense_rank (use dense_rank because some product_name have the same number of orders)
+-- Step 4: Take out the most purchased item by dense_rank = 1 
 with table_group as (
     select product_name, customer_id,
         count(product_id) as number_orders_by_customer
@@ -49,11 +56,14 @@ from (
     select *,
             dense_rank() over (order by total_orders desc) as row_number
     from table_sum
-) as table_row
+) as table_row 
 where row_number = 1
 
 --------------------------------------------------------------------------------------------
-/* Which item was the most popular for each customer? */
+-- 5.Which item was the most popular for each customer?
+-- Step 1: Count number of orders per product_name
+-- Step 2: Ranking number of orders by row_number() partition by customer_id 
+-- Step 3: Take out the most popular item for each customer by row_number = 1
 with table_group as (  
     select customer_id, product_name,
         count(product_id) as number_orders
@@ -63,18 +73,21 @@ with table_group as (
         left join menu 
             on sales.product_id = menu.product_id
     ) as table_join
-    group by customer_id, product_name
-)
+    group by customer_id, product_name 
+) -- group customer_id & product_name and count number of orders per product
 select *
 from (
     select *,
         row_number() over (partition by customer_id order by number_orders desc) as row_number
     from table_group
-) as table_row
+) as table_row -- ranking number of orders of customers
 where row_number = 1
 
 --------------------------------------------------------------------------------------------
-/* Which item was purchased first by the customer after they became a member? */ 
+-- 6.Which item was purchased first by the customer after they became a member? 
+-- Step 1: Join three tables and remove customers who are not member of restaurant
+-- Step 2: Calculate distance of join_date and order_date to distinguish before and after became a member orders (>0 is after member and <0 is before)
+-- Step 3: Take out the data which have distance > 0 and use row_number to rank the distance -> rank = 1 is the first orders after became a member
 with table_join as (
     select sales.customer_id, order_date, product_name, join_date
     from sales 
@@ -99,7 +112,10 @@ from (
 where rank = 1
 
 --------------------------------------------------------------------------------------------
-/* Which item was purchased just before the customer became a member? */
+-- 7.Which item was purchased just before the customer became a member? 
+-- Step 1: Join three tables and remove customers who are not member of restaurant
+-- Step 2: Calculate distance of join_date and order_date to distinguish before and after became a member orders (>0 is after member and <0 is before)
+-- Step 3: Take out the data which have distance < 0 and use rank function to rank the distance -> rank = 1 is the first orders after became a member
 with table_join as (
     select sales.customer_id, order_date, product_name, join_date
     from sales 
@@ -111,7 +127,7 @@ with table_join as (
 )
 , table_dis as (
     select *,
-        datediff(day,order_date, join_date) as distance
+        datediff(day, join_date, order_date) as distance
     from table_join
 )
 select *
@@ -119,12 +135,15 @@ from (
     select *,
         rank() over (partition by customer_id order by distance asc) as rank
     from table_dis 
-    where distance > 0
+    where distance < 0
 ) as table_row 
 where rank = 1
 
 --------------------------------------------------------------------------------------------
-/* What is the total items and amount spent for each member before they became a member? */
+-- 8.What is the total items and amount spent for each member before they became a member?
+-- Step 1: Join three tables and remove customers who are not member of restaurant
+-- Step 2: Calculate distance of join_date and order_date to distinguish before and after became a member orders (>0 is after member and <0 is before)
+-- Step 3: Take out the data which have distance < 0 and GROUP BY customer, calculate total amount, number of items
 with table_join as (
     select sales.customer_id, order_date, product_name, join_date, price
     from sales 
@@ -136,18 +155,20 @@ with table_join as (
 )
 , table_dis as (
     select *,
-        datediff(day,order_date, join_date) as distance
+        datediff(day, join_date, order_date) as distance
     from table_join
 )
 select customer_id,
     sum(price) as total_amount,
     count(product_name) as number_items
 from table_dis
-where distance > 0 
+where distance < 0 
 group by customer_id
 
 --------------------------------------------------------------------------------------------
-/* If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have? */ 
+-- 9.If each $1 spent equates to 10 points and sushi has a 2x points multiplier - how many points would each customer have? 
+-- Step 1: Add in new column point
+-- Step 2: GROUP BY customer and calculate total point
 select customer_id,
     sum([point]) as total_point
 from (
@@ -160,8 +181,11 @@ from (
 group by customer_id
 
 --------------------------------------------------------------------------------------------
-/* In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi, 
-how many points do customer A and B have at the end of January? */ 
+-- 10.In the first week after a customer joins the program (including their join date) they earn 2x points on all items, not just sushi, how many points do customer A and B have at the end of January?
+-- Step 1: Join three tables, filter orders in January and remove customers who are not member of restaurant
+-- Step 2: Calculate distance of join_date and order_date to distinguish before and after became a member orders (>0 is after member and <0 is before)
+-- Step 3: Add in new column point with condition about distance, price and product_name
+-- Step 4: GROUP BY customer and calculate total point
 with table_join as (
     select sales.customer_id, order_date, join_date, product_name, price
     from sales 
